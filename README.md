e444bd3ca607598f59cd5637e2b0c8c711883f85
Theophilus <theondukwe@gmail.com>
2025-03-27 18:33:00 -0400

    Project completed. will go back to style it later.

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-# 📊 Project: Complex API
+# 📊 Project: Complex API - METADATA IOS APPSTORE
 
 ### Goal: Use data returned from one api to make a request to another api and display the data returned
 
-### How to
+### Documentation - https://app-store-metadata-api.kula.app/?ref=freepublicapis.com
+
+App Store Metadata
+
+- Get real-time metadata for any iOS app in seconds.
+
+Data retrieved: App name, iphone screenshot, description of app, genre
+
+Data from first api that made request to second API is the "genre"
--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
-// FUCNTION & Behaviour
+// FUNCTION & Behaviour
+
+
+document.querySelector('button').addEventListener('click', searchApp)
+function searchApp() {
+    let inputVal = document.querySelector('input').value
+    const apiKey = 'qzEtNF4HyjCcEm8rDsO3E48eRnyaWzRJCsyu0hxRHhMoxWaEwOsK6hyI'
+
+    let url = `https://app-store-metadata-api.kula.app/api/v1/apple/lookup-app?bundle_id=com.apple.${inputVal}`
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        let appId = data.data.id
+        console.log(appId)
+        fetch(`https://app-store-metadata-api.kula.app/api/v1/apple/apps/${appId}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                document.querySelector('#h1-1').innerText = data.data.name
+                document.querySelector('#description').innerText = data.data.description
+                document.querySelector('img').src = data.data.screenshots.iphone[1].url
+                let genres = data.data.genres[0]
+                const myHeaders = new Headers();
+                    myHeaders.append('Authorization', apiKey);
+
+                        const requestOptions = {
+                        method: 'GET',
+                        headers: myHeaders,
+
+};
+            fetch(`https://api.pexels.com/v1/search?query=${genres}`, requestOptions)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data)
+                        document.querySelector('#img-news').src = data.photos[0].src.large
+         })       
+            
+            })
+            .catch(err => {
+                console.log(`error${err}`)
+            })
+
+    })
+
+}
